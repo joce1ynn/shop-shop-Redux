e1148d5657980d6a27ac1b98428d733ff07c6d06
Wenwen Tian <wwtian9@gmail.com>
2023-02-10 15:02:54 -0700

    add comments on reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -7,13 +7,18 @@ import {
   UPDATE_CURRENT_CATEGORY,
 } from "./actions";
 
+// a reducer function should always return a new state, rather than mutating the original state.
 export const reducer = (state, action) => {
   switch (action.type) {
     // if action type value is the value of `UPDATE_PRODUCTS`,
     // return a new state object with an updated products array
     case UPDATE_PRODUCTS:
       return {
+        //...state spread operator creates a shallow copy of the original state, 
         ...state,
+        //[...action.products] creates a shallow copy of the action.products array
+        // These copies ensure that the original state and the original `products` array are not mutated directly, 
+        // but rather new objects are created for the updated state and updated products array.
         products: [...action.products],
       };
 
@@ -26,6 +31,8 @@ export const reducer = (state, action) => {
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
+        // currentCategory is a single value (most likely an ID), and not an array, 
+        // so there's no need to create a shallow copy of it using the spread operator 
         currentCategory: action.currentCategory,
       };
 
