f5697b4e2bfca8f4d916906464316402373b7f5a
Wenwen Tian <wwtian9@gmail.com>
2023-04-20 11:59:24 -0600

    add checkout function

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,10 +4,29 @@ import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+// we can't call useQuery(QUERY_CHECKOUT)
+// The useQuery Hook is meant to run when a component is first rendered, 
+// not at a later point in time based on a user action like a button click.
+// useLazyQuery Hook can be declared like any other Hook but won't actually execute until you tell it to
+import { useLazyQuery } from '@apollo/client';
 import './style.css';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    //该data变量将包含结帐会话，但仅在使用该getCheckout()函数调用查询之后。
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            })
+        }
+    }, [data])
 
     // when refresh pages, the items in cart still there!
     useEffect(() => {
@@ -33,6 +52,20 @@ const Cart = () => {
         return sum.toFixed(2)
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id)
+            }
+        })
+
+        getCheckout({
+            variables: { products: productIds }
+        })
+    }
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -54,7 +87,7 @@ const Cart = () => {
                     <div className='flex-row space-between'>
                         <strong>Total: ${calculateTotal()}</strong>
                         {Auth.loggedIn()
-                            ? <button>Checkout</button>
+                            ? <button onClick={submitCheckout}>Checkout</button>
                             : <span>(log in to check out)</span>}
                     </div>
                 </div>)
